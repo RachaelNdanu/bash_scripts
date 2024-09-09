4a6003eeb026378208f5b5eaa2039a07c74726f3
Rachael <rndanu102@gmail.com>
2024-09-09 15:29:47 +0300

    first commit

---
+++ cal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prompt the user to enter a year
+echo "Enter a year:"
+read year
+
+# Display the calendar of the given year
+ncal $year
---
+++ cores.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Use the nproc command to get the number of CPU cores
+cpu_cores=$(nproc)
+
+# Print the number of CPU cores
+echo "Number of CPU cores: $cpu_cores"
