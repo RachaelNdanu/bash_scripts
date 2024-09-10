7f2f590db42db796c80f38e9a168e25858929c48
Rachael <rndanu102@gmail.com>
2024-09-10 16:35:15 +0300

    more

---
+++ find.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find / -iname $1 2>/dev/null
+
+#find / -iname "$1" 2>/dev/null
---
+++ special.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "The name of the scripts is: $0"
+echo "The number of arguments is: $#"
+echo "The values of all arguments is: $@"
