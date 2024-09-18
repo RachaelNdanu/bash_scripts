ab3281072786c90a3527bb7f792ad3e2b893c5ca
Rachael <rndanu102@gmail.com>
2024-09-18 14:47:38 +0300

    changesss

---
+++ user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+user=("Rachael" 829 "sudo,fsociety" "zsh")
+  
+echo "username: ${user[0]}"
+echo "UID: ${user[1]}"
+echo "user-groups: ${user[2]}"
+echo "default login shell: ${user[3]}"
