b62ffbf1f6add193828a616bf2be91c34807ef71
Rachael <rndanu102@gmail.com>
2024-09-24 12:35:04 +0300

    moreeeee

---
+++ char.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CHAR=$1
+
+case $CHAR in
+	[a-z])
+		echo "$CHAR is a small alphabet" ;;
+	[A-Z])
+                echo "$CHAR is a big alphabet" ;;
+	[0-9])
+                echo "$CHAR is a digit";;
+	*)
+                echo "$CHAR is a special character";;
+esac
+
+
+
+
+
+
+
+
