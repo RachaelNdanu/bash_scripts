01bbda623f61c3a0296f91715a886a8daf0826cf
Rachael <rndanu102@gmail.com>
2024-09-30 15:08:17 +0300

    more1

---
+++ function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+func () {
+	echo "$1 is the first argument to func() "
+        echo "$2 is the first argument to func() "
+}
+
+func cool code
+
+echo "$1 is the first argument to the script"
+echo "$2 is the first argument to the script"
---
+++ loc-glob.sh
@@ -0,0 +1,15 @@
+v1='A'
+v2='B'
+
+myfunc () {
+	local v1='C'
+	v2='D'
+	echo "inside my function v1:$v1 , v2:$v2"
+}
+
+
+echo "before calling myfunc() v1:$v1 , v2:$v2"
+
+myfunc
+
+echo "After calling myfunc() v1:$v1 , v2:$v2"
