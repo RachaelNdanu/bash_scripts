98ed5aa4e310030560023eb016e8b8783c2f0478
Rachael <rndanu102@gmail.com>
2025-01-04 13:52:40 +0300

    gigz

--- giga2mega.sh
+++ giga2mega.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 GIGA=$1
+if [ "$GIGA" -lt 0 ]; then
+    echo "Error: The number of gigabytes cannot be negative."
+    exit 1
+fi
+
 MEGA=$(($GIGA * 1024))
 
 echo "$1 GB is equal to $MEGA MB"
+
